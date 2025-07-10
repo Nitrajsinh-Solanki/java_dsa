375ef991a50f22b0baa6f3af5eccfd250791ea0e
Nitrajsinh-Solanki <nrsolanki2005@gmail.com>
2025-07-10 11:12:16 +0530

    added one pattern

---
+++ patterns/pattern22.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class pattern22 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int num = sc.nextInt();
+        
+
+        for(int i = 1 ; i<= num ; i++){
+            char ch = 'A';
+            for(int k = num - i ; k>= 1 ; k--){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<= i ; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+
+/*
+
+    A
+   AB
+  ABC
+ ABCD
+ABCDE
+
+ */
